becb5564fd0e1cf1dc4cbf28cbc8b2faa7102674
rajesh <kemar.dev1@outlook.com>
2023-01-08 23:37:45 +0530

    First Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StructuralDirective1';
+
+  condition = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnlessDirective
   ],
   imports: [
     BrowserModule
